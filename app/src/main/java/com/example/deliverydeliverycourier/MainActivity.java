82c2d9e716b7a8366dc0677e5cd5b6deb2039bc0
Richu <richujain1995@gmail.com>
2020-07-30 23:43:50 -0400

    Registeration and login done

--- app/src/main/java/com/example/deliverydeliverycourier/MainActivity.java
+++ app/src/main/java/com/example/deliverydeliverycourier/MainActivity.java
@@ -1,24 +1,35 @@
 package com.example.deliverydeliverycourier;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.rengwuxian.materialedittext.MaterialEditText;
 
+import Model.User;
+
 public class MainActivity extends AppCompatActivity {
     //
 
     Button btnSignin, btnRegister;
+    RelativeLayout rootLayout;
 
     FirebaseAuth auth;
     FirebaseDatabase db;
@@ -41,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
         //init Firebase
         auth = FirebaseAuth.getInstance();
         db = FirebaseDatabase.getInstance();
-        users = db.getReference("users");
+        users = db.getReference("drivers");
 
 
 
@@ -49,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
 
         btnRegister = (Button)findViewById(R.id.btnRegister);
         btnSignin = (Button)findViewById(R.id.btnSignIn);
+        rootLayout = (RelativeLayout)findViewById(R.id.rootLayout);
 
         //Event
 
@@ -63,22 +75,97 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+btnSignin.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        showLoginDialog();
+    }
+});
+
+    }
+
+    private void showLoginDialog() {
+
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Sign In");
+        dialog.setMessage("Please use email to Sign In");
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View login_layout = inflater.inflate(R.layout.layout_register,null);
+        final MaterialEditText edtEmail = login_layout.findViewById(R.id.edtEmail);
+        final MaterialEditText edtPassword = login_layout.findViewById(R.id.edtPassword);
 
 
+        dialog.setView(login_layout);
+
+        //set button
+
+        dialog.setPositiveButton("Sign In", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+
+                        //Checking Validation
+                        if (TextUtils.isEmpty(edtEmail.getText().toString())) {
+                            Snackbar.make(rootLayout, "Please Enter email address", Snackbar.LENGTH_SHORT).show();
+                            return;
+                        }
+
+                        if (TextUtils.isEmpty(edtPassword.getText().toString())) {
+                            Snackbar.make(rootLayout, "Please Enter Password", Snackbar.LENGTH_SHORT).show();
+                            return;
+                        }
+
+                        if (edtPassword.getText().toString().length() < 6) {
+
+                            Snackbar.make(rootLayout, "Password too short", Snackbar.LENGTH_SHORT).show();
+                            return;
+                        }
+
+                        //login
+                        auth.signInWithEmailAndPassword(edtEmail.getText().toString(),edtPassword.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                startActivity(new Intent(MainActivity.this, Welcome.class ));
+                                finish();
+
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Snackbar.make(rootLayout,"Failed"+e.getMessage(),Snackbar.LENGTH_SHORT).show();
+                            }
+                        });
+
+
+                    }
+
+                });
+
+
+        dialog.setNegativeButton("Cancell", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
+
+        dialog.show();
     }
 
     private void showRegisterDialogue()
     {
-        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
         dialog.setTitle("Register");
         dialog.setMessage("Please use email to register");
 
         LayoutInflater inflater = LayoutInflater.from(this);
          View register_layout = inflater.inflate(R.layout.layout_register,null);
-        MaterialEditText edtEmail = register_layout.findViewById(R.id.edtEmail);
-        MaterialEditText edtPassword = register_layout.findViewById(R.id.edtPassword);
-        MaterialEditText edtName = register_layout.findViewById(R.id.edtName);
-        MaterialEditText edtPhone = register_layout.findViewById(R.id.edtPhone);
+        final MaterialEditText edtEmail = register_layout.findViewById(R.id.edtEmail);
+        final MaterialEditText edtPassword = register_layout.findViewById(R.id.edtPassword);
+        final MaterialEditText edtName = register_layout.findViewById(R.id.edtName);
+        final MaterialEditText edtPhone = register_layout.findViewById(R.id.edtPhone);
 
 
         dialog.setView(register_layout);
@@ -89,10 +176,76 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
             dialogInterface.dismiss();
+
+        //Checking Validation
+                if(TextUtils.isEmpty(edtEmail.getText().toString()))
+                {
+                    Snackbar.make(rootLayout,"Please Enter email address", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(edtPhone.getText().toString()))
+                {
+                    Snackbar.make(rootLayout,"Please Enter Phone Number", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(edtPassword.getText().toString()))
+                {
+                    Snackbar.make(rootLayout,"Please Enter Password", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(edtPassword.getText().toString().length()<6)
+                {
+
+                    Snackbar.make(rootLayout,"Password too short", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+
+                //Registering a new user
+                auth.createUserWithEmailAndPassword(edtEmail.getText().toString(),edtPassword.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+//Save user to DB
+
+                        User user = new User();
+                        user.setEmail(edtEmail.getText().toString());
+                        user.setName(edtName.getText().toString());
+                        user.setPhone(edtPhone.getText().toString());
+                        user.setPassword(edtPassword.getText().toString());
+
+//use email to key
+                        users.child(user.getEmail())
+                                .setValue(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+
+                                Snackbar.make(rootLayout,"Registered Successfully", Snackbar.LENGTH_SHORT).show();
+
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Snackbar.make(rootLayout," Registration Failed"+e.getMessage(), Snackbar.LENGTH_SHORT).show();
+                            }
+                        });
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Snackbar.make(rootLayout,"  Failed"+e.getMessage(), Snackbar.LENGTH_SHORT).show();
+
+                    }
+                });
+
+
             }
         });
 
-        dialog.setNegativeButton("Cancell", new DialogInterface.OnClickListener() {
+        dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 dialogInterface.dismiss();
@@ -101,7 +254,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
+        dialog.show();
 
 
     }
