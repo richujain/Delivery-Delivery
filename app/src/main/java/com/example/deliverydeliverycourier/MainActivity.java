bfa4ee627325ee9be0bc4e7374aadf5abe9214f2
Richu <richujain1995@gmail.com>
2020-07-30 20:57:47 -0400

    Register Page update 2

--- app/src/main/java/com/example/deliverydeliverycourier/MainActivity.java
+++ app/src/main/java/com/example/deliverydeliverycourier/MainActivity.java
@@ -1,15 +1,30 @@
 package com.example.deliverydeliverycourier;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class MainActivity extends AppCompatActivity {
     //
 
     Button btnSignin, btnRegister;
+
+    FirebaseAuth auth;
+    FirebaseDatabase db;
+    DatabaseReference users;
+
+
 
 
 
@@ -23,10 +38,67 @@ public class MainActivity extends AppCompatActivity {
         finish();
 
 
-        //init
+        //init Firebase
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseDatabase.getInstance();
+        users = db.getReference("users");
+
+
+
+        //init view
 
         btnRegister = (Button)findViewById(R.id.btnRegister);
         btnSignin = (Button)findViewById(R.id.btnSignIn);
+
+        //Event
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showRegisterDialogue();
+            }
+
+
+        });
+
+
+
+
+
+    }
+
+    private void showRegisterDialogue()
+    {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Register");
+        dialog.setMessage("Please use email to register");
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+         View register_layout = inflater.inflate(R.layout.layout_register,null);
+        MaterialEditText edtEmail = register_layout.findViewById(R.id.edtEmail);
+        MaterialEditText edtPassword = register_layout.findViewById(R.id.edtPassword);
+        MaterialEditText edtName = register_layout.findViewById(R.id.edtName);
+        MaterialEditText edtPhone = register_layout.findViewById(R.id.edtPhone);
+
+
+        dialog.setView(register_layout);
+
+        //set button
+
+        dialog.setPositiveButton("Register", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+            dialogInterface.dismiss();
+            }
+        });
+
+        dialog.setNegativeButton("Cancell", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
 
 
 
