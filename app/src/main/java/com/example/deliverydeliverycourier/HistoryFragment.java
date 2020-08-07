38feee3b219f2c1dff632e96022ba45d4d36305b
Richu Jain <richujain1995@gmail.com>
2020-08-07 04:07:01 -0400

    Displaying total amount earned by the driver.

--- app/src/main/java/com/example/deliverydeliverycourier/HistoryFragment.java
+++ app/src/main/java/com/example/deliverydeliverycourier/HistoryFragment.java
@@ -1,14 +1,29 @@
 package com.example.deliverydeliverycourier;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,9 +37,71 @@ public class HistoryFragment extends Fragment {
     private String mParam2;
 /*********************************************************************************************/
 
+    RecyclerView recyclerViewHistory;
+
+    private ArrayList<ModelClass> arrayList;
+    private RecyclerAdapterForHistory recyclerAdapterForHistory;
+    private Context context;
+    DatabaseReference databaseReference;
+    FirebaseUser firebaseUser;
+    FirebaseAuth mAuth;
+    String uid = "";
+    Double totalAmount = 0.0;
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
+        firebaseUser = mAuth.getCurrentUser();
+        this.uid = firebaseUser.getUid();
+        recyclerViewHistory = getView().findViewById(R.id.recyclerViewHistory);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recyclerViewHistory.setLayoutManager(layoutManager);
+        recyclerViewHistory.setHasFixedSize(true);
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        arrayList = new ArrayList<>();
+        clearAll();
+        getDataFromFirebase();
+
+    }
+    private void getDataFromFirebase() {
+        Query query = databaseReference.child("orders");
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                clearAll();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    if(snapshot.child("completedby").getValue().toString().equals(uid)){
+                        String key = snapshot.getKey();
+                        ModelClass modelClass = new ModelClass();
+                        modelClass.setKey(key);
+                        modelClass.setImageurl(snapshot.child("imageurl").getValue().toString());
+                        modelClass.setDate(snapshot.child("date").getValue().toString());
+                        modelClass.setOfferedAmount(snapshot.child("amount").getValue().toString());
+                        modelClass.setStatus(snapshot.child("status").getValue().toString());
+                        Double amount = Double.parseDouble(snapshot.child("amount").getValue().toString());
+                        totalAmount = totalAmount + amount;
+                        Log.d("total",""+totalAmount);
+                        arrayList.add(modelClass);
+                    }
+                }
+                recyclerAdapterForHistory = new RecyclerAdapterForHistory(getContext(),arrayList);
+                recyclerViewHistory.setAdapter(recyclerAdapterForHistory);
+                recyclerAdapterForHistory.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+    private void clearAll(){
+        if(arrayList != null){
+            arrayList.clear();
+            if(recyclerAdapterForHistory != null){
+                recyclerAdapterForHistory.notifyDataSetChanged();
+            }
+        }
+        arrayList = new ArrayList<>();
     }
 
     /*********************************************************************************************/
---
+++ app/src/main/java/com/example/deliverydeliverycourier/RecyclerAdapterForHistory.java
@@ -0,0 +1,86 @@
+package com.example.deliverydeliverycourier;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class RecyclerAdapterForHistory extends RecyclerView.Adapter<RecyclerAdapterForHistory.ViewHolder> {
+    private static final String TAG = "RecyclerView";
+    private Context context;
+    private ArrayList<ModelClass> modelClassArrayList;
+
+    public RecyclerAdapterForHistory(Context context, ArrayList<ModelClass> modelClassArrayList) {
+        this.context = context;
+        this.modelClassArrayList = modelClassArrayList;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerAdapterForHistory.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recyclerview_row,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerAdapterForHistory.ViewHolder holder, final int position) {
+        holder.statusOfOrder.setText(modelClassArrayList.get(position).getStatus());
+        holder.offeredAmount.setText(modelClassArrayList.get(position).getOfferedAmount());
+        holder.orderDate.setText(modelClassArrayList.get(position).getDate());
+
+        Glide.with(context).load(modelClassArrayList.get(position).getImageurl())
+                .into(holder.vehicleTypeCircularImage);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //get key somehow
+                String key = modelClassArrayList.get(position).getKey();
+                //context.startActivity(new Intent(context,ViewOrderDetails.class));
+                Intent intent = new Intent(context,ViewOrderDetails.class);
+                intent.putExtra("key",key);
+                context.startActivity(intent);
+            }
+        });
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                Toast.makeText(context, "Position is "+position, Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return modelClassArrayList.size();
+    }
+
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        //widgets
+        ImageView vehicleTypeCircularImage;
+        TextView statusOfOrder, orderDate, offeredAmount;
+
+        public ViewHolder(View itemView){
+            super(itemView);
+            vehicleTypeCircularImage = itemView.findViewById(R.id.vehicleTypeCircularImage);
+            statusOfOrder = itemView.findViewById(R.id.statusOfOrder);
+            orderDate = itemView.findViewById(R.id.orderDate);
+            offeredAmount = itemView.findViewById(R.id.offeredAmount);
+        }
+    }
+}
